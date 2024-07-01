b076e4d6586a1bdbd56b0855b41a0399254fe8c9
Taffarel Campos <taffarel_lc@hotmail.com>
2024-07-01 16:19:06 -0300

    Criação projeto

---
+++ app/src/main/java/com/example/instagramapp/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.instagramapp.login.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagramapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
