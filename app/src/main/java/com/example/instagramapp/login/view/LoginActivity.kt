224e01b91678b283a57afbd7218c8f7317dfa4fe
Taffarel Campos <taffarel_lc@hotmail.com>
2024-07-12 00:23:17 -0300

    Validando campos InputText

--- app/src/main/java/com/example/instagramapp/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instagramapp/login/view/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.instagramapp.login.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagramapp.R
+import com.example.instagramapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginEdtEmailInput.error = "Esse e-mail é invalido"
     }
 }
