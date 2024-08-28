c4a65a1fdfc98875d31debda081464ccb1398312
Taffarel Campos <taffarel_lc@hotmail.com>
2024-08-27 23:06:09 -0300

    Animação de Scrolling com NestedScrollView e CoordinatorLayout

--- app/src/main/java/com/example/instagramapp/profile/view/FragmentProfile.kt
+++ app/src/main/java/com/example/instagramapp/profile/view/FragmentProfile.kt
@@ -4,16 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instagramapp.R
+import com.example.instagramapp.databinding.FragmentProfileBinding
 
 class FragmentProfile : Fragment() {
+    private var profileBinding: FragmentProfileBinding? = null
+    private val binding get() = profileBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        profileBinding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = binding.profileRv
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_profile_grid, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        private class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(image: Int) {
+               itemView.findViewById<ImageView>(R.id.item_profile_img_grid).setImageResource(image)
+            }
+        }
     }
 }
