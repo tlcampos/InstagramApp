24249b0fed28b3feee775b60b4c255e7f872e686
Taffarel Campos <taffarel_lc@hotmail.com>
2024-09-04 01:36:34 -0300

    Toolbar customizada e com menus

--- app/src/main/java/com/example/instagramapp/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instagramapp/main/view/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.instagramapp.main.view
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.example.instagramapp.R
+import com.example.instagramapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        val toolbar = binding.mainToolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 }
--- app/src/main/java/com/example/instagramapp/profile/view/FragmentProfile.kt
+++ app/src/main/java/com/example/instagramapp/profile/view/FragmentProfile.kt
@@ -2,6 +2,8 @@ package com.example.instagramapp.profile.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -31,6 +33,16 @@ class FragmentProfile : Fragment() {
         val rv = binding.profileRv
         rv.layoutManager = GridLayoutManager(requireContext(), 3)
         rv.adapter = PostAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_profile, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
