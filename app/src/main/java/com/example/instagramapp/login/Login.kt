f754772384b89961e2c75aefe0805bb75c435536
Taffarel Campos <taffarel_lc@hotmail.com>
2024-09-13 23:24:22 -0300

    Implementando Arquitetura MVP

---
+++ app/src/main/java/com/example/instagramapp/login/Login.kt
@@ -0,0 +1,11 @@
+package com.example.instagramapp.login
+
+interface Login {
+
+    //View
+    interface View {
+        fun showProgress(enabled: Boolean)
+        fun displayEmailFailure(emailError: Int?)
+        fun displayPasswordFailure(passwordError: Int?)
+    }
+}
