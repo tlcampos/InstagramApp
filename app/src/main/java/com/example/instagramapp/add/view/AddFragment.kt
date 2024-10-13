b1a544a67d8b317697558bb311ac56bcd64d3691
Taffarel Campos <taffarel_lc@hotmail.com>
2024-10-13 01:45:54 -0300

    Gerenciamento de permissões

--- app/src/main/java/com/example/instagramapp/add/view/AddFragment.kt
+++ app/src/main/java/com/example/instagramapp/add/view/AddFragment.kt
@@ -1,5 +1,11 @@
 package com.example.instagramapp.add.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.instagramapp.R
 import com.example.instagramapp.add.Add
 import com.example.instagramapp.common.base.BaseFragment
@@ -27,5 +33,36 @@ class AddFragment : BaseFragment<FragmentAddBinding, Add.Presenter>(
                 tab.text = getString(adapter.tabs[position])
             }.attach()
         }
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            getPermission.launch(REQUIRED_PERMISSION)
+        }
     }
+
+    private fun startCamera() {
+        Log.i("Teste","startCamera")
+    }
+
+    private val getPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.permission_denied), Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION) == PackageManager.PERMISSION_GRANTED
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+
+    }
+
 }
