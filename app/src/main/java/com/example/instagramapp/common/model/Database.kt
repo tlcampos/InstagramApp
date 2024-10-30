753e56a569d34a1eb7c53dbc5edd1453428cdeb5
Taffarel Campos <taffarel_lc@hotmail.com>
2024-10-29 22:16:00 -0300

    Atividade de Publicação

--- app/src/main/java/com/example/instagramapp/common/model/Database.kt
+++ app/src/main/java/com/example/instagramapp/common/model/Database.kt
@@ -3,16 +3,29 @@ package com.example.instagramapp.common.model
 import java.util.UUID
 
 object Database {
+
     val usersAuth = hashSetOf<UserAuth>()
     val photos = hashSetOf<Photo>()
-    val posts = hashMapOf<String, Set<Post>>()
-    val feeds = hashMapOf<String, Set<Post>>()
+    val posts = hashMapOf<String, MutableSet<Post>>()
+    val feeds = hashMapOf<String, MutableSet<Post>>()
+    val followers = hashMapOf<String, Set<String>>()
 
     var sessionAuth: UserAuth? = null
 
     init {
-        usersAuth.add(UserAuth(UUID.randomUUID().toString(),"UserA","userA@gmail.com", "12345678"))
-        usersAuth.add(UserAuth(UUID.randomUUID().toString(),"UserB","userB@gmail.com", "87654321"))
+        val userA = UserAuth(UUID.randomUUID().toString(), "UserA", "userA@gmail.com", "12345678")
+        val userB = UserAuth(UUID.randomUUID().toString(), "UserB", "userB@gmail.com", "87654321")
+
+        usersAuth.add(userA)
+        usersAuth.add(userB)
+
+        followers[userA.uuid] = hashSetOf()
+        posts[userA.uuid] = hashSetOf()
+        feeds[userA.uuid] = hashSetOf()
+
+        followers[userB.uuid] = hashSetOf()
+        posts[userB.uuid] = hashSetOf()
+        feeds[userB.uuid] = hashSetOf()
 
         sessionAuth = usersAuth.first()
     }
--- app/src/main/java/com/example/instagramapp/common/model/UserAuth.kt
+++ app/src/main/java/com/example/instagramapp/common/model/UserAuth.kt
@@ -1,7 +1,7 @@
 package com.example.instagramapp.common.model
 
 data class UserAuth(
-    val uuid: String,
+    var uuid: String,
     val name: String,
     val email: String,
     val password: String,
